29f0001ae783140bf30a1dce55f7a3873091bd73
BN <dev@gmail.com>
2022-10-28 19:20:29 +0530

    Added random number program

---
+++ random-number/RandomNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string outputFolder = $@"{Directory.GetCurrentDirectory()}\Output\";
+        if (!Directory.Exists(outputFolder))
+            Directory.CreateDirectory(outputFolder);
+
+        string filePath = $"{outputFolder}random-numbers.txt";
+
+        try
+        {
+            List<int> numbers = new List<int>();
+            var data = "";
+
+            Random number = new Random();
+            for (int i = 1; i <= 100000; i++)
+            {
+                while (true)
+                {
+                    int genNumber = (int)number.NextInt64(1, 9999999);
+                    if (!numbers.Contains(genNumber))
+                    {
+                        numbers.Add(genNumber);
+                        data += $"{genNumber}\n";
+                        break;
+                    }
+                }
+            }
+
+            File.AppendAllText(filePath, data);
+        }
+        catch (Exception Ex)
+        {
+            Console.WriteLine(Ex.ToString());
+        }
+    }
+}
+
+      
